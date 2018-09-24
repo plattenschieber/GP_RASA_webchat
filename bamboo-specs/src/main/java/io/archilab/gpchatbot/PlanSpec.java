ea7bc910b28c4bddfd706638ea50dd3487dc4b04
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2018-09-24 14:49:39 +0200

    Fix specs

--- bamboo-specs/src/main/java/io/archilab/gpchatbot/PlanSpec.java
+++ bamboo-specs/src/main/java/io/archilab/gpchatbot/PlanSpec.java
@@ -29,87 +29,81 @@ import com.atlassian.bamboo.specs.util.BambooServer;
 @BambooSpec
 public class PlanSpec {
 
-    public Plan plan() {
-        final Plan plan = new Plan(new Project()
-            .oid(new BambooOid("ky5ricqu8qv5"))
-            .key(new BambooKey("CHAT"))
-            .name("Chatbot"),
-            "webchat",
-            new BambooKey("WEB"))
-            .oid(new BambooOid("kxw2ardmf0u9"))
-            .pluginConfigurations(new ConcurrentBuilds()
-                .useSystemWideDefault(false))
-            .stages(new Stage("Default Stage")
-                .jobs(new Job("Default Job",
-                    new BambooKey("JOB1"))
-                    .artifacts(new Artifact()
-                            .name("docker-compose-prod")
-                            .copyPattern("docker-compose.prod.yaml")
-                            .location("./docker")
-                            .shared(true)
-                            .required(true),
-                        new Artifact()
-                            .name("docker-compose")
-                            .copyPattern("docker-compose.yaml")
-                            .location("./docker")
-                            .shared(true)
-                            .required(true))
-                    .tasks(new VcsCheckoutTask()
-                            .description("Checkout the repository")
-                            .checkoutItems(new CheckoutItem().defaultRepository()),
-                        new ScriptTask()
-                            .description("Create commit hash variable file")
-                            .inlineBody("echo \"commit-hash=$(date +%s%N)\" > ./commit-hash"),
-                        new InjectVariablesTask()
-                            .description("Inject the commit hash variable")
-                            .path("./commit-hash")
-                            .namespace("inject")
-                            .scope(InjectVariablesScope.RESULT),
-                        new DockerBuildImageTask()
-                            .description("Build the Docker image")
-                            .imageName("docker.nexus.gpchatbot.archi-lab.io/chatbot/webchat")
-                            .useCache(true)
-                            .dockerfileInWorkingDir(),
-                        new ScriptTask()
-                            .description("Tag the Docker image with commit hash")
-                            .inlineBody("docker tag docker.nexus.gpchatbot.archi-lab.io/chatbot/webchat docker.nexus.gpchatbot.archi-lab.io/chatbot/webchat:${bamboo.inject.commit-hash}"),
-                        new DockerPushImageTask()
-                            .customRegistryImage("docker.nexus.gpchatbot.archi-lab.io/chatbot/webchat")
-                            .defaultAuthentication(),
-                        new DockerPushImageTask()
-                            .customRegistryImage("docker.nexus.gpchatbot.archi-lab.io/chatbot/webchat:${bamboo.inject.commit-hash}")
-                            .defaultAuthentication(),
-                        new ScriptTask()
-                            .description("Remove old images from Nexus Docker repository")
-                            .inlineBody("echo \"# Nexus Credentials\\nnexus_host = \\\"https://nexus.gpchatbot.archi-lab.io\\\"\\nnexus_username = \\\"bamboo\\\"\\nnexus_password = \\\"gpchatbot\\\"\\nnexus_repository = \\\"docker-hosted\\\"\" > .credentials\nnexus-cli image delete -name chatbot/webchat -keep 21"))
-                    .requirements(new Requirement("system.builder.command.nexus-cli"))))
-            .linkedRepositories("chatbot-webchat (master)")
+  public Plan plan() {
+    final Plan plan = new Plan(new Project().oid(new BambooOid("ky5ricqu8qv5"))
+        .key(new BambooKey("CHAT")).name("Chatbot"), "webchat", new BambooKey("WEB"))
+        .oid(new BambooOid("kxw2ardmf0u9"))
+        .pluginConfigurations(new ConcurrentBuilds().useSystemWideDefault(false))
+        .stages(new Stage("Default Stage")
+            .jobs(new Job("Default Job", new BambooKey("JOB1")).artifacts(
+                new Artifact().name("docker-compose-prod")
+                    .copyPattern("docker-compose.prod.yaml")
+                    .location("./docker").shared(true).required(true),
+                new Artifact().name("docker-compose")
+                    .copyPattern("docker-compose.yaml")
+                    .location("./docker").shared(true).required(true))
+                .tasks(new VcsCheckoutTask()
+                        .description("Checkout the repository")
+                        .checkoutItems(
+                            new CheckoutItem().defaultRepository()),
+                    new ScriptTask()
+                        .description(
+                            "Create commit hash variable file")
+                        .inlineBody(
+                            "echo \"commit-hash=$(date +%s%N)\" > ./commit-hash"),
+                    new InjectVariablesTask()
+                        .description(
+                            "Inject the commit hash variable")
+                        .path("./commit-hash").namespace("inject")
+                        .scope(InjectVariablesScope.RESULT),
+                    new DockerBuildImageTask()
+                        .description("Build the Docker image")
+                        .imageName(
+                            "docker.nexus.gpchatbot.archi-lab.io/chatbot/webchat")
+                        .useCache(true).dockerfileInWorkingDir(),
+                    new ScriptTask().description(
+                        "Tag the Docker image with commit hash")
+                        .inlineBody(
+                            "docker tag docker.nexus.gpchatbot.archi-lab.io/chatbot/webchat docker.nexus.gpchatbot.archi-lab.io/chatbot/webchat:${bamboo.inject.commit-hash}"),
+                    new DockerPushImageTask().customRegistryImage(
+                        "docker.nexus.gpchatbot.archi-lab.io/chatbot/webchat")
+                        .defaultAuthentication(),
+                    new DockerPushImageTask().customRegistryImage(
+                        "docker.nexus.gpchatbot.archi-lab.io/chatbot/webchat:${bamboo.inject.commit-hash}")
+                        .defaultAuthentication(),
+                    new ScriptTask().description(
+                        "Remove old images from Nexus Docker repository")
+                        .inlineBody(
+                            "echo \"# Nexus Credentials\\nnexus_host = \\\"https://nexus.gpchatbot.archi-lab.io\\\"\\nnexus_username = \\\"bamboo\\\"\\nnexus_password = \\\"gpchatbot\\\"\\nnexus_repository = \\\"docker-hosted\\\"\" > .credentials\nnexus-cli image delete -name chatbot/webchat -keep 21"))
+                .requirements(new Requirement(
+                    "system.builder.command.nexus-cli"))))
+        .linkedRepositories("chatbot-webchat (master)")
 
-            .triggers(new BitbucketServerTrigger())
-            .planBranchManagement(new PlanBranchManagement()
-                .delete(new BranchCleanup())
-                .notificationForCommitters());
-        return plan;
-    }
+        .triggers(new BitbucketServerTrigger())
+        .planBranchManagement(new PlanBranchManagement().delete(new BranchCleanup())
+            .notificationForCommitters());
+    return plan;
+  }
 
-    public PlanPermissions planPermission() {
-        final PlanPermissions planPermission = new PlanPermissions(new PlanIdentifier("CHAT", "WEB"))
-            .permissions(new Permissions()
-                .userPermissions("bamboo", PermissionType.ADMIN, PermissionType.VIEW, PermissionType.CLONE, PermissionType.BUILD, PermissionType.EDIT)
-                .loggedInUserPermissions(PermissionType.VIEW)
-                .anonymousUserPermissionView());
-        return planPermission;
-    }
+  public PlanPermissions planPermission() {
+    final PlanPermissions planPermission =
+        new PlanPermissions(new PlanIdentifier("CHAT", "WEB")).permissions(new Permissions()
+            .userPermissions("bamboo", PermissionType.ADMIN, PermissionType.VIEW,
+                PermissionType.CLONE, PermissionType.BUILD, PermissionType.EDIT)
+            .loggedInUserPermissions(PermissionType.VIEW)
+            .anonymousUserPermissionView());
+    return planPermission;
+  }
 
-    public static void main(String... argv) {
-        //By default credentials are read from the '.credentials' file.
-        BambooServer bambooServer = new BambooServer("https://bamboo.gpchatbot.archi-lab.io");
-        final PlanSpec planSpec = new PlanSpec();
+  public static void main(String... argv) {
+    // By default credentials are read from the '.credentials' file.
+    BambooServer bambooServer = new BambooServer("https://bamboo.gpchatbot.archi-lab.io");
+    final PlanSpec planSpec = new PlanSpec();
 
-        final Plan plan = planSpec.plan();
-        bambooServer.publish(plan);
+    final Plan plan = planSpec.plan();
+    bambooServer.publish(plan);
 
-        final PlanPermissions planPermission = planSpec.planPermission();
-        bambooServer.publish(planPermission);
-    }
+    final PlanPermissions planPermission = planSpec.planPermission();
+    bambooServer.publish(planPermission);
+  }
 }
