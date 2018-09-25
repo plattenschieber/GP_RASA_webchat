0e9b369a320519b143c448bcd0862b196e2dff2e
Jann Deterling <jann.deterling@smail.th-koeln.de>
2018-09-25 11:36:15 +0200

    add bug fix for training

--- src/components/Widget/components/Training/index.js
+++ src/components/Widget/components/Training/index.js
@@ -10,22 +10,23 @@ import Header from '../Conversation/components/Header';
 import connect from 'react-redux/es/connect/connect';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { SHOW_ACTIONS, SHOW_EVENTS, SHOW_INTENTS } from '../../../../store/training-actions';
+import Intent from './components/Intents/components/Intent';
 
 const Training = props =>
   <div className="training-container">
     <Header title={props.trainings[0]} subtitle={'Wähle das passende Item!'}/>
     {
-      props.trainings.map((training) => {
+      props.trainings.map((training, index) => {
         console.log(training);
         switch (training.component) {
           case SHOW_INTENTS:
-            return <Intents/>;
+            return <Intents key={index} />;
           case SHOW_ACTIONS:
-            return <Scores/>;
+            return <Scores key={index} />;
           case SHOW_EVENTS:
-            return <Events/>;
+            return <Events key={index} />;
           default:
-            return <Intents/>;
+            return <Intents key={index} />;
         }
       })
     }
