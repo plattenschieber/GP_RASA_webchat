b3c3cb2cb0b7c4e1d5f924777aba25d5ecaef3a6
Jann Deterling <jann.deterling@smail.th-koeln.de>
2018-10-01 12:39:06 +0200

    adds print to default.conf in setenv.sh

--- nginx/setenv.sh
+++ nginx/setenv.sh
@@ -3,6 +3,6 @@ echo "Replace ENV in default.conf"
 envsubst '${ROOT}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
 
 ls $ROOT
-cat /etc/conf.d/default.conf
+cat /etc/nginx/conf.d/default.conf
 
 echo "Will start nginx now!"
