49f7e987d07d40e170a3563f4aae778bbfc7f821
Marco Reitano <marco.reitano@th-koeln.de>
2018-10-01 13:04:48 +0200

    Adds echo for root variable

--- nginx/setenv.sh
+++ nginx/setenv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Replace ENV in default.conf"
+echo $ROOT
 envsubst '${ROOT}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
 
 cat /etc/nginx/conf.d/default.conf
