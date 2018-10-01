19316dccb60710f01879c408a86fc707646de577
Marco Reitano <marco.reitano@th-koeln.de>
2018-10-01 13:07:47 +0200

    Cat for default.conf

--- nginx/setenv.sh
+++ nginx/setenv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Replace ENV in default.conf"
 echo $ROOT
+cat /etc/nginx/conf.d/default.conf
 envsubst '${ROOT}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
 
 cat /etc/nginx/conf.d/default.conf
